3a2d1ff1b4ac4ea5c0b20c3c8f2d252d494a3555
Shane Elliott <shane@Shanes-MBP.home>
2020-06-07 07:56:48 -0700

    looking at request info

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get('/', function (request, response) {
 })
 
 app.post('/appt', function (request, response) {
+    console.log(request)
     console.log(request.body)
     var currentTime  = new Date();
     var date1 = new Date()
